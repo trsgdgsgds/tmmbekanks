a9a72d27b2baacaef3d21bb6a6bae0dd6a34c720
trsgdgsgds <74206732+trsgdgsgds@users.noreply.github.com>
2021-01-19 17:44:01 +0300

    Add files via upload

---
+++ app/bot.js
@@ -0,0 +1,211 @@
+const Discord = require("discord.js");
+require("events").EventEmitter.defaultMaxListeners = 30000;
+  require("events").defaultMaxListeners = 30000;
+const client = new Discord.Client();
+const ayarlar = require("./ayarlar.json");
+const kanal = ayarlar.kanal;
+const fs = require("fs");
+require("./util/eventLoader")(client);
+const express = require("express");
+const app = express();
+const http = require("http");
+var Jimp = require('jimp');
+app.get("/", (request, response) => {
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
+
+const log = message => {
+  console.log(` => { ${message} } `);
+  
+};
+
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+fs.readdir("./komutlar/", (err, files) => {
+  if (err) console.error(err);
+  log(`${files.length} komut yüklenecek.`);
+  files.forEach(f => {
+    let props = require(`./komutlar/${f}`);
+    log(`AKTİF: ${props.help.name}.`);
+    client.commands.set(props.help.name, props);
+    props.conf.aliases.forEach(alias => {
+      client.aliases.set(alias, props.help.name);
+    });
+  });
+});
+
+client.reload = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./komutlar/${command}`)];
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+client.load = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+client.unload = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./komutlar/${command}`)];
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+////////////////////////
+
+client.elevation = message => {
+  if (!message.guild) {
+    return;
+  }
+  let permlvl = 0;
+  if (message.member.hasPermission("ADMINISTRATOR")) permlvl = 3;
+  return permlvl;
+};
+
+
+
+
+
+
+client.on("ready", () => {//splashen
+  client.user.setPresence({
+    game: { name: `SPLASHEN V12 PUBLİC BOT ALTYAPI`, type: "WATCHING" },
+    status: "online"
+  });
+});
+
+
+
+// İSİM YAŞ İSİM DEĞİŞTİRME 
+
+client.on("guildMemberAdd", member => {
+  let tag = ayarlar.tag;
+  //splashen
+  member.setNickname(`${tag} İsim • Yaş`);
+});
+
+// İSİM YAŞ İSİM DEĞİŞTİRME SON
+
+
+
+
+
+
+//BOT ROLÜ
+
+client.on(`guildMemberAdd`, async member => {//splashen
+  let botrol = ayarlar.botROL;
+if(!member.bot) return;
+member.roles.add(botrol)
+})
+
+// BOT ROLÜ SON
+
+
+
+
+// kayıtsız rolü
+
+client.on(`guildMemberAdd`, async member => {
+  let kayıtsızROL = ayarlar.kayıtsızROL;
+if(member.bot) return;
+member.roles.add(kayıtsızROL)
+})
+
+/// kayıtsız rolü son
+//splashen
+
+
+
+//splashen
+
+// BOT OTOROL
+
+client.on('guildMemberAdd', async member => {//splashen
+if(member.user.bot) {
+  const botROL = ayarlar.botROL
+member.roles.add(botROL)
+}
+})
+// GİRİŞ 
+  client.on("guildMemberAdd", member => { 
+    const moment = require('moment');
+  const kanal = ayarlar.giriskanal;
+  let user = client.users.cache.get(member.id);
+  require("moment-duration-format");
+    const tarih = new Date().getTime() - user.createdAt.getTime();  
+  const embed = new Discord.MessageEmbed()
+  let rol = ayarlar.kayıtsızROL
+ member.roles.add(rol)//splashen
+
+  var kontrol;
+if (tarih < 1296000000) kontrol = '<a:no1:756946138342621295> Bu Kullanıcı **Şüpheli**'
+if (tarih > 1296000000) kontrol = '<a:tik3:756946140825649214> Bu Kullanıcı **Güvenli**'
+  moment.locale("tr");
+  let kanal1 = client.channels.cache.find(x => x.id === kanal);
+    let giris = new Discord.MessageEmbed()
+    .setDescription(`
+ » • <:new_partner_badge:781900500022853632> Hoşgeldin ${member}
+ 
+ » • <a:pembeh:751553654561046619> Seninle birlikte **${member.guild.memberCount}** kişiyiz.
+ 
+ » • [ **${ayarlar.tag}** ] Tagımızı alarak ekibimize katılabilirsin.
+ 
+ » • <a:alarm1:756946152938799225> <@&${ayarlar.yetkiliROL}> rolündekiler seninle ilgilenecektir.
+ 
+ » •   ${kontrol} 
+ 
+ » • <a:sari3:751558669585612830> Hesabın Oluşturulma Tarihi: \n • \` ${moment(member.user.createdAt).format("YYYY DD MMMM dddd (hh:mm:ss)")} \`
+ 
+ » • <a:duyur:766652129678721074> Ses teyit odasında kaydınızı yaptırabilirsiniz. 
+
+`)//splashen
+    .setImage('https://i.pinimg.com/originals/8c/9a/07/8c9a079986a4ce112882fea6db3ffdee.gif')
+    .setTimestamp()
+    
+      client.channels.cache.find(x => x.id === kanal).send(`<@&${ayarlar.yetkiliROL}>`)
+client.channels.cache.find(x => x.id === kanal).send(giris)
+    
+  });
+// GİRİŞ SON
+//splashen
+
+client.login(ayarlar.token);
---
+++ app/events/ready.js
@@ -0,0 +1,11 @@
+const chalk = require("chalk");
+const moment = require("moment");
+const Discord = require("discord.js");
+const ayarlar = require("../ayarlar.json");
+
+let prefix = ayarlar.prefix;
+
+module.exports = client => {
+  client.user.setStatus("online");
+console.log('𝗕𝗼𝘁 𝘀𝗽𝗹𝗮𝘀𝗵𝗲𝗻 𝘁𝗮𝗿𝗮𝗳ı𝗻𝗱𝗮𝗻 𝘆𝗮𝗽ı𝗹𝗺ış𝘁ı𝗿.𝗣𝗮𝘆𝗹𝗮𝘀ı𝗹𝗺𝗮𝘀ı 𝘆𝗮𝘀𝗮𝗸𝘁ı𝗿.𝗔𝘆𝗮𝗿𝗹𝗮𝗿ı 𝗮𝘆𝗮𝗿𝗹𝗮𝗿.𝗷𝘀𝗼𝗻 𝗱𝗼𝘀𝘆𝗮𝘀ı𝗻𝗱𝗮𝗻 𝘆𝗮𝗽𝗮𝗯𝗶𝗹𝗶𝗿𝘀𝗶𝗻𝗶𝘇.𝗧ü𝗺 𝗮𝘆𝗮𝗿𝗹𝗮𝗿ı 𝗱𝗼𝗹𝗱𝘂𝗿𝗺𝗮𝘀𝘀𝗮𝗻ı𝘇 𝗯𝗮𝘇ı ö𝘇𝗲𝗹𝗹𝗶𝗸𝗹𝗲𝗿 ç𝗮𝗹ış𝗺𝗮𝘆𝗮𝗯𝗶𝗹𝗶𝗿.');
+};
